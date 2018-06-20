65114f22803806a9ea13c6829270102b1b163525
AMSolovyev <alexander.solovyev7@gmail.com>
2018-06-20 21:34:02 +0300

    The script checks site condition

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+The script ```check_sites_health.py``` checks urls in the file and shows a site condition.
+If the site has a response 200 it is OK. The script ```check_sites_health.py```
+also checks paid days.
+
+# How to use
+```
+1. virtualenv -p python3 env
+2. source env/bin/activate
+3. pip install -r requirements.txt
+4. python check_sites_health.py -f -f sites.txt
+```
+
+You can output data like this:
+```
+OK: domain: devman.org has 68 paid days
+OK: domain: www.rbc.ru has 40 paid days
+OK: domain: vk.com has 367 paid days
+OK: domain: ecfor.ru has 247 paid days
+OK: domain: kremlin.ru has 405 paid days
+OK: domain: ok.ru has 162 paid days
+OK: domain: vkusvill.ru has 216 paid days
+OK: domain: hh.ru has 283 paid days
+OK: domain: www.superjob.ru has 252 paid days
+OK: domain: www.python.org has 280 paid days
+```
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,79 @@
-def load_urls4check(path):
-    pass
+import whois
+import datetime
+import argparse
+import requests
+from urllib.parse import urlparse
+from requests import ConnectionError, Timeout
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-f',
+        '--filepath',
+        help='Input the filepath to the file'
+    )
+    return parser.parse_args()
+
+
+def load_sites_from_file(filepath):
+    with open(filepath, 'r') as file_with_url:
+        url_list = file_with_url.read().splitlines()
+    return url_list
+
+
+def get_domain_name(url):
+    domain_name = urlparse(url).netloc
+    return domain_name
+
 
 def is_server_respond_with_200(url):
-    pass
+    try:
+        response = requests.get(url)
+    except ConnectionError:
+        return False, 'ConnectionError'
+    except Timeout:
+        return False, 'Timeout'
+    if response is None:
+        return False, None
+    status = response.status_code
+    if status == 200:
+        return True, status
+    return False, status
 
-def get_domain_expiration_date(domain_name):
-    pass
+
+def get_expiration_date(domain_name):
+    expiration_date = whois.whois(domain_name).expiration_date
+    if isinstance(expiration_date, list):
+        return expiration_date[0]
+    else:
+        return expiration_date
+
+
+def check_domain_during_date(expiration_date):
+    today = datetime.datetime.today()
+    during_date = (expiration_date - today).days
+    return during_date
+
+
+def print_url_and_domain(during_date, min_paid_period):
+    if during_date <= min_paid_period:
+        print('Attention: domain {} has {} days. It is bad'.format(
+            domain_name, during_date))
+    else:
+        print('OK: domain: {0} has {1} paid days'.format(
+            domain_name, during_date))
+
 
 if __name__ == '__main__':
-    pass
+    min_paid_period = 30
+
+    args = get_parser()
+    url_list = load_sites_from_file(args.filepath)
+
+    for url in url_list:
+        domain_name = get_domain_name(url)
+        server_respond_with_200 = is_server_respond_with_200(url)
+        expiration_date = get_expiration_date(domain_name)
+        domain_during_date = check_domain_during_date(expiration_date)
+        print_url_and_domain(domain_during_date, min_paid_period)
